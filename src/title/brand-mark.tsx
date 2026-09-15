c36a7c5ed085220df174802f86938d3d83c8bdeb
Kris Baumgartner <kjbaumgartner@gmail.com>
2026-09-15 13:06:13 +0200

    🐛 Correct PMNDRS corner logo proportions

--- src/title/brand-mark.tsx
+++ src/title/brand-mark.tsx
@@ -16,10 +16,8 @@ export function BrandMark() {
       aria-hidden={hidden}
     >
       <rect width="100" height="100" fill="#000000" />
-      <rect x="41.5" y="29.5" width="27" height="41" fill="#ffffff" />
-      <rect x="27" y="41.5" width="31" height="30" fill="#000000" />
       <path
-        d="M28.75 43.75h12.5v12.5h-12.5z M43.75 43.75h12.5v12.5h-12.5z M43.75 58.75h12.5v12.5h-12.5z"
+        d="M43.75 28.75h27.5v27.5h-12.5v-15h-15z M28.75 43.75h12.5v12.5h-12.5z M43.75 43.75h12.5v12.5h-12.5z M43.75 58.75h12.5v12.5h-12.5z"
         fill="#ffffff"
       />
     </svg>
